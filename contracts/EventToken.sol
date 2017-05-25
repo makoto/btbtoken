d5fe88b5505686f5ebe47391109491bb820a6e04
Makoto Inoue <inouemak@googlemail.com>
2017-05-26 05:59:06 +0900

    Add identity

--- contracts/EventToken.sol
+++ contracts/EventToken.sol
@@ -17,11 +17,36 @@ import './zeppelin/token/LimitedTransferToken.sol';
 contract EventToken is MintableToken, LimitedTransferToken {
   uint public constant unit = 1;
   uint public constant decimals = 0;
+  event TokenClaimed(address indexed _to, bytes32 identity);
+  mapping (address => bytes32) public ownerToIdentity;
+  mapping (bytes32 => address) public identityToOwner;
 
-  modifier canTransfer(address _sender, uint _value) {
+  modifier canTransfer(address _sender, address _to, uint _value) {
    if (_value != unit) throw;
    if (_value > transferableTokens(_sender, uint64(now))) throw;
    _;
+   var identity = ownerToIdentity[_sender];
+   identityToOwner[identity] = _to;
+   ownerToIdentity[_to] = identity;
+   delete ownerToIdentity[_sender];
+  }
+
+  modifier onlyUnique(bytes32 identity){
+    if (identity == 0x0) throw;
+    if (identityToOwner[identity] != 0x0) throw;
+    _;
+  }
+
+  function issue(address _to) onlyOwner {
+    mint(owner, unit);
+    approve(_to, unit);
+  }
+
+  function claim(bytes32 identity) onlyUnique(identity) {
+    transferFrom(owner, msg.sender, unit);
+    ownerToIdentity[msg.sender] = identity;
+    identityToOwner[identity] = msg.sender;
+    TokenClaimed(msg.sender, identity);
   }
 
   function isTokenOwner(address _owner) constant returns (bool) {
