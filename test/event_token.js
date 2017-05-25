dcfb6247500f79c52bac735a7f45eb2704add544
Makoto Inoue <inouemak@googlemail.com>
2017-05-26 05:59:26 +0900

    Fix spec

--- test/event_token.js
+++ test/event_token.js
@@ -1,15 +1,46 @@
 require('babel-polyfill');
-var EventToken = artifacts.require("./EventToken.sol");
+const EventToken = artifacts.require("./EventToken.sol");
+let token, issuer, ether_card, participant_wallet, identity;
 
 contract('EventToken', function(accounts) {
+  beforeEach(async function(){
+    token = await EventToken.new();
+    issuer = accounts[0];
+    ether_card = accounts[1];
+    participant_wallet = accounts[2];
+    identity = 'makoto';
+  })
+
   it("has a unit", async function() {
-    let token = await EventToken.new();
     assert.strictEqual((await token.unit.call()).toNumber(), 1);
   })
-  it("can mint by owner", async function() {
-    let token = await EventToken.new();
-    await token.mint(accounts[1], 1);
-    assert.strictEqual((await token.balanceOf.call(accounts[1])).toNumber(), 1);
-    assert.strictEqual((await token.isTokenOwner.call(accounts[1])), true);
+
+  it("has a decimal", async function() {
+    assert.strictEqual((await token.decimals.call()).toNumber(), 0);
   })
+
+  it("can transfer identity", async function(){
+    await token.issue(ether_card, {from:issuer});
+    await token.claim(web3.fromUtf8(identity), {from:ether_card});
+    assert.strictEqual((await token.isTokenOwner.call(ether_card)), true);
+    assert.strictEqual(web3.toUtf8(await token.ownerToIdentity.call(ether_card)), identity);
+
+    await token.transfer(participant_wallet, 1, {from:ether_card});
+    assert.strictEqual((await token.isTokenOwner.call(participant_wallet)), true);
+    assert.strictEqual(web3.toUtf8(await token.ownerToIdentity.call(participant_wallet)), identity);
+    assert.strictEqual(await token.identityToOwner.call(web3.fromUtf8(identity)), participant_wallet);
+  })
+
+  it("cannot claim the same identity", async function(){
+    await token.issue(ether_card, {from:issuer});
+    await token.claim(web3.fromUtf8(identity), {from:ether_card});
+    await token.issue(participant_wallet, {from:issuer});
+    await token.claim(web3.fromUtf8(identity), {from:participant_wallet}).catch(function(){});
+    assert.strictEqual((await token.isTokenOwner.call(participant_wallet)), false);
+    assert.strictEqual(web3.toUtf8(await token.ownerToIdentity.call(participant_wallet)), '');
+    let owner = await token.identityToOwner.call(web3.fromUtf8(identity))
+    assert.strictEqual(owner, ether_card);
+  })
+
+  // it("cannot transfer to existing owner")
 })
