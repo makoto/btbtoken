9d929ca0a208505c8e61ebdb2abcf0f8a91da252
Makoto Inoue <inouemak@googlemail.com>
2017-05-28 20:39:24 +0900

    Fix bug

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,8 +4,9 @@ class Form extends Component {
   render(){
     if (this.props.user.status == 'claimable') {
       submitButton = (<button type="button" name="button">Claim</button>)
+    }else{
+      submitButton = null;
     }
-
     return(
       <form className="claim">
         <input style={{width: '350px'}} type="text" name="token_owner_address" value={this.props.user.address}></input>
